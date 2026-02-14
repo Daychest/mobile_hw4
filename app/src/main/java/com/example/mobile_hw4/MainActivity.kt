aa5dfe2c0019d9db04a05cc833524164c58e0ec1
Daychest <veikka.paivarinta@gmail.com>
2026-02-14 21:11:49 +0200

    save

--- app/src/main/java/com/example/mobile_hw4/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw4/MainActivity.kt
@@ -3,45 +3,34 @@ package com.example.mobile_hw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobile_hw4.ui.theme.Mobile_hw4Theme
+import com.plcoding.notificationsguide.ui.theme.NotificationsGuideTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        val service = CounterNotificationService(applicationContext)
         setContent {
-            Mobile_hw4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Mobile_hw4Theme() {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Button(onClick = {
+                        service.showNotification(Counter.value)
+                    }) {
+                        Text(text = "Show notification")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobile_hw4Theme {
-        Greeting("Android")
-    }
-}
