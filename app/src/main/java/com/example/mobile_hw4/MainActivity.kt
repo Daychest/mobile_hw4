6007a733846cd23195b3609ca71af71f0d237765
Daychest <veikka.paivarinta@gmail.com>
2026-02-16 01:38:38 +0200

    Added proximity sensor

--- app/src/main/java/com/example/mobile_hw4/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw4/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.mobile_hw4
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobile_hw4.ui.theme.Mobile_hw4Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +37,7 @@ class MainActivity : ComponentActivity() {
         val service = CounterNotificationService(applicationContext)
         setContent {
             Mobile_hw4Theme() {
+                ProximitySensor()
                 Box(modifier = Modifier.fillMaxSize()) {
                     Button(onClick = {
                         service.showNotification(Counter.value)
@@ -30,6 +47,48 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ProximitySensor() {
+    val ctx = LocalContext.current
+    val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val proximitySensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+    val sensorStatus = remember {
+        mutableStateOf("")
+    }
+    val proximitySensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    sensorStatus.value = "Near"
+                } else {
+                    sensorStatus.value = "Away"
+                }
+            }
+        }
+    }
+
+    sensorManager.registerListener(
+        proximitySensorEventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = sensorStatus.value,
+            fontSize = 40.sp, modifier = Modifier.padding(5.dp)
+        )
     }
 }
 
