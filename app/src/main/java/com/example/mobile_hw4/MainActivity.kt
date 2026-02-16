3e97f96ffbdf21070d8f055b2b278a05b84f2711
Daychest <veikka.paivarinta@gmail.com>
2026-02-16 23:08:27 +0200

    Made notification permission texts more accurate

--- app/src/main/java/com/example/mobile_hw4/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw4/MainActivity.kt
@@ -9,17 +9,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,7 +22,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
@@ -91,11 +84,11 @@ fun PermissionThing(){
 
     when (viewModel.state) {
         PermissionState.Granted -> {
-            Text("Record audio permission granted!")
+            Text("Notification permission granted!")
         }
 
         PermissionState.DeniedAlways -> {
-            Text("Permission was permanently declined.")
+            Text("Notification permission was permanently declined.")
             Button(onClick = {
                 controller.openAppSettings()
             }) {
@@ -105,10 +98,10 @@ fun PermissionThing(){
         else -> {
             Button(
                 onClick = {
-                    viewModel.provideOrRequestRecordAudioPermission()
+                    viewModel.provideOrRequestRecordNotificationPermission()
                 }
             ) {
-                Text("Request permission")
+                Text("Request notification permission")
             }
         }
     }
@@ -169,7 +162,7 @@ class PermissionsViewModel(
         }
     }
 
-    fun provideOrRequestRecordAudioPermission() {
+    fun provideOrRequestRecordNotificationPermission() {
         viewModelScope.launch {
             try {
                 controller.providePermission(Permission.REMOTE_NOTIFICATION)
